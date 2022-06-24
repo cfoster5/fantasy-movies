18f19364505e996d3e0254732c09cd139368c33e
cfoster5 <cfoster3204@gmail.com>
2022-06-23 20:37:22 -0500

    Continued restructure after babel config changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@/components": "./src/components",
+            "@/constants": "./src/constants",
+            "@/hooks": "./src/hooks",
+            "@/navigation": "./src/navigation",
+            "@/providers": "./src/providers",
+            "@/screens": "./src/screens",
+            "@/types": "./src/types",
+          },
+        },
+      ],
+    ],
   };
 };
--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -1,9 +1,10 @@
 import * as WebBrowser from "expo-web-browser";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
-import Colors from "../constants/Colors";
 import { MonoText } from "./StyledText";
 import { Text, View } from "./Themed";
+
+import Colors from "@/constants/Colors";
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
