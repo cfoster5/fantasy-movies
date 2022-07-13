5df4667902926366f77dd1c68569868994d93551
cfoster5 <cfoster3204@gmail.com>
2022-07-13 10:17:26 -0500

    Initializing firebase

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
+import { initializeApp } from "firebase/app";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
 
+import { firebaseConfig } from "@/config";
 import { AppProvider } from "@/providers/app";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  initializeApp(firebaseConfig);
 
   if (!isLoadingComplete) {
     return null;
