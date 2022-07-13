89dc81c7e6d18c2342c4f28d624291c6971ff010
cfoster5 <cfoster3204@gmail.com>
2022-07-13 10:20:03 -0500

    Fix for missing "idb" module from firebase

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// https://stackoverflow.com/a/72201722/5648619
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
